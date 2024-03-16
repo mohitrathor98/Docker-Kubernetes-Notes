b1c69f4fc4d8bb779cd26a5506b5c1633417934b
mohitrathor98 <mohitrathor8@gmail.com>
2024-03-16 19:55:49 +0530

    Python Application is complete

--- Week3-Volumes-And-Bind-Mounts/Project-for-volume-exp/app.py
+++ Week3-Volumes-And-Bind-Mounts/Project-for-volume-exp/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import os
+
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -8,14 +10,26 @@ def home():
     return render_template('feedback.html')
 
 
+@app.route('/exists')
+def exist():
+    return render_template('exists.html')
+
 @app.route('/create', methods=['POST'])
 def submit():
     if request.method == 'POST':
         feedback_title = request.form['title']
         feedback_text = request.form['text']
-        print(feedback_title)
-        print(feedback_text)
-        return feedback_title + ' ' + feedback_text
+
+        with open(f"temp/{feedback_title}.txt", 'w') as fd:
+            fd.write(feedback_text)
+
+        if f"{feedback_title}.txt" in os.listdir('feedback'):
+            return redirect('/exists')
+
+        with open(f"feedback/{feedback_title}.txt", 'w') as fd:
+            fd.write(feedback_text)
+
+        return redirect('/')
 
 
 if __name__ == "__main__":
--- Week3-Volumes-And-Bind-Mounts/Project-for-volume-exp/readme.md
+++ Week3-Volumes-And-Bind-Mounts/Project-for-volume-exp/readme.md
@@ -4,8 +4,8 @@
 - [X] Run it using flask
 - [X] How to capture submit?
 
-- [ ] Save the captured data in the temp directory
-- [ ] Check if the file with title as named is present in feedback, if not save the feedback there.
+- [X] Save the captured data in the temp directory
+- [X] Check if the file with title as named is present in feedback, if not save the feedback there.
 
 - [ ] Create a dockerfile to do this stuff.
 
