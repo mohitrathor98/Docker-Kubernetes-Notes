97578689b78fcaaa25270f97c4bd5db0850371e7
mohitrathor98 <mohitrathor8@gmail.com>
2024-03-16 19:33:32 +0530

    Able to capture the contents of text fields after submit action for the feedback python application

--- Week3-Volumes-And-Bind-Mounts/Project-for-volume-exp/app.py
+++ Week3-Volumes-And-Bind-Mounts/Project-for-volume-exp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('feedback.html')
+
+
+@app.route('/create', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        feedback_title = request.form['title']
+        feedback_text = request.form['text']
+        print(feedback_title)
+        print(feedback_text)
+        return feedback_title + ' ' + feedback_text
 
 
 if __name__ == "__main__":
