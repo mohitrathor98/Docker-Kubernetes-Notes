be5cde7bdae75602edb902b3710abed45254c796
mohitrathor98 <mohitrathor8@gmail.com>
2024-03-24 09:04:34 +0530

    Mongo db implementation done

--- Week4-Networking-Outside-and-Between-containers/Python-Application-For-Container-Communication/app.py
+++ Week4-Networking-Outside-and-Between-containers/Python-Application-For-Container-Communication/app.py
@@ -1,19 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+from pymongo import MongoClient
+# pymongo needs to be installed
 
 import requests
 
 app = Flask(__name__)
-
-
-def get_data_from_swapi(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        json_data = response.json()
-        return json_data
-    except Exception as e:
-        print("ERROR: ", e)
-        return None
 
 
 @app.route('/')
@@ -23,13 +14,14 @@ def home():
 
 @app.route('/fav', methods=['GET'])
 def get_favs():
-    return "<h3>Give the favs here</h3>"
+    data = mongo_interactions()
+    return f"<h3>{data}</h3>"
 
 
 @app.route('/fav', methods=['POST'])
 def post_favs():
     item_data = request.json
-    return jsonify(item_data)
+    return mongo_interactions(item_data)
 
 
 @app.route('/movies', methods=['GET'])
@@ -45,6 +37,46 @@ def get_people():
     data = get_data_from_swapi('https://swapi.dev/api/people/')
     name = data["results"][0]["name"]
     return f"<h3>Your people is --- {name}</h3>"
+
+
+def get_data_from_swapi(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        json_data = response.json()
+        return json_data
+    except Exception as e:
+        print("ERROR: ", e)
+        return None
+
+
+def mongo_interactions(data=None):
+    client = MongoClient('mongodb://172.17.0.2:27017/')
+    db = client['favorites']
+    if data is None:
+        return get_data(db)
+    else:
+        return post_data(db, data)
+
+
+def get_data(db):
+    data = []
+    for col in ['movie', 'people']:
+        collection = db[col]
+        cursor = collection.find()
+        for document in cursor:
+            data.append(document)
+    return data
+
+
+def post_data(db, data):
+    if 'movie' in data.keys():
+        collection = db['movie']
+    else:
+        collection = db['people']
+
+    result = collection.insert_one(data)
+    return f"Inserted the data. ID: {result.inserted_id}"
 
 
 if __name__ == "__main__":
