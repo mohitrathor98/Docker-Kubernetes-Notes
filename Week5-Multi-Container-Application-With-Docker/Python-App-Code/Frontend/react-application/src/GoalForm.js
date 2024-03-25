15933f2dd05b460c1cd883ad8e37c73b90dec9b7
mohitrathor98 <mohitrathor8@gmail.com>
2024-03-25 10:18:14 +0530

    Added the TODO list buttons and labels

--- Week5-Multi-Container-Application-With-Docker/Python-App-Code/Frontend/react-application/src/GoalForm.js
+++ Week5-Multi-Container-Application-With-Docker/Python-App-Code/Frontend/react-application/src/GoalForm.js
@@ -1,32 +1,53 @@
 import React, { useState } from 'react';
-
 import './GoalForm.css';
 
 function GoalForm() {
   const [goal, setGoal] = useState('');
-
+  const [goalsList, setGoalsList] = useState([]);
+  
   const handleGoalChange = (event) => {
     setGoal(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('Goal submitted:', goal);
-    setGoal('');
+    if (goal.trim() !== '') {
+      setGoalsList([...goalsList, goal]);
+      setGoal('');
+    }
+  };
+
+  const handleClearGoal = (index) => {
+    const updatedGoalsList = goalsList.filter((_, i) => i !== index);
+    setGoalsList(updatedGoalsList);
   };
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <label htmlFor="goalInput">What do you want to do?</label>
-      <input
-        type="text"
-        id="goalInput"
-        value={goal}
-        onChange={handleGoalChange}
-        placeholder="What's on your mind?"
-      />
-      <button type="submit">Add Goal</button>
-    </form>
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="goalInput">Goal Tracker</label>
+        <input
+          type="text"
+          id="goalInput"
+          value={goal}
+          onChange={handleGoalChange}
+          placeholder="Enter your goal"
+        />
+        <button type="submit">What do you want to do?</button>
+      </form>
+    
+      {goalsList.length === 0 ? (
+        <p className="no-goals-label">No Goals for Today</p>
+      ) : (
+        <div>
+          {goalsList.map((goal, index) => (
+            <button id="goal-buttons" key={index} onClick={() => handleClearGoal(index)}>
+              {goal}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
 
