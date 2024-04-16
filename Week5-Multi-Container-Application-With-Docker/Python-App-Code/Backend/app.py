f4510478f9947ecaaf1e714126a8154e52148137
mohitrathor98 <mohitrathor8@gmail.com>
2024-04-16 08:30:49 +0530

    adding logging for get, post events

--- Week5-Multi-Container-Application-With-Docker/Python-App-Code/Backend/app.py
+++ Week5-Multi-Container-Application-With-Docker/Python-App-Code/Backend/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request
 
 import json
-
+import logging
 import mongo_interaction
 
 app = Flask(__name__)
+
+logging.basicConfig(filename='requests.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 @app.route('/')
@@ -12,13 +15,15 @@ def home():
     return "An Application to Store, View and Delete Goals"
 
 
-@app.route('/sample')
+@app.route('/sample', methods=['GET'])
 def sample():
+    log_request(request)
     return json.dumps({'goal': "goal-name"})
 
 
 @app.route('/store', methods=['POST'])
 def store():
+    log_request(request)
     if request.is_json:
         return mongo_interaction.Mongo_Connect("store", request.json)
     else:
@@ -27,15 +32,21 @@ def store():
 
 @app.route('/view', methods=['GET'])
 def view():
+    log_request(request)
     return mongo_interaction.Mongo_Connect("view")
 
 
 @app.route('/delete', methods=['POST'])
 def delete():
+    log_request(request)
     if request.is_json:
         return mongo_interaction.Mongo_Connect("delete", request.json)
     else:
         return json.dumps({'error': 'Request must contain JSON data'})
+
+
+def log_request(req):
+    logging.info(f"{req.method} request to {req.url} with data: {req.data}")
 
 
 if __name__ == "__main__":
